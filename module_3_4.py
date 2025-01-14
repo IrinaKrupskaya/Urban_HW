2d2d005224084e8cfa6be67f8b896c6221fcde0f
IrinaKrupskaya <rin.cosmostars@gmail.com>
2025-01-14 22:00:48 +0400

    Add files via upload

---
+++ module_3_4.py
@@ -0,0 +1,42 @@
+# def test_func(*params):
+#     print('Тип', type(params))
+#     print('Аргумент', params)
+#
+# test_func(1, 2, 3, 4)
+
+# def summator(txt, *values, type="sum"):
+#     s = 0
+#     for i in values:
+#         s += i
+#     return f'{txt}{s} {type}'
+#
+# print(summator('сумма чисел:', 2, 3, 4, type='summator'))
+
+# def info(value, *types, name_au='Den', **values):
+#     print('Тип', type(values))
+#     print('Аргумент', values)
+#     for key, value in values.items():
+#         print(key, value)
+#     print(types)
+# info('Пример использования параметров всех типов', 1, 2, 3, name_au = "denis", name='Den', course='python')
+
+# def my_sum(n, *args, txt='Сумма чисел'):
+#     s = 0
+#     for i in range(len(args)):
+#         s += args[i] ** n
+#     print(txt + ':', s)
+#
+# my_sum(1, 1,2,3,4,5)
+
+def single_root_words(root_word, *other_words):
+    same_words = []
+    root_word_lower = root_word.lower()
+    for word in other_words:
+        if root_word_lower in word.lower():
+            same_words.append(word)
+    return same_words
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+
+print(result1)
+print(result2)
