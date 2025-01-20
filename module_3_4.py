6e2847866f76bb13f109d466c57b78b7dd746d2d
IrinaKrupskaya <rin.cosmostars@gmail.com>
2025-01-20 17:16:30 +0400

    Update module_3_4.py

--- module_3_4.py
+++ module_3_4.py
@@ -1,12 +1,15 @@
-def single_root_words(root_word, *other_words):
+ef single_root_words(root_word, *other_words):
     same_words = []
-    root_word_lower = root_word.lower()
-    for word in other_words:
-        if root_word_lower in word.lower():
-            same_words.append(word)
+    root_word_lower = root_word.lower()                                #исключаем влияние регистра переводя всё в нижний
+    for i in other_words:
+        other_words_lower = i.lower()
+        if (root_word_lower in other_words_lower                       #проверка наличия слова в составе другого из списка
+                or other_words_lower in root_word_lower):
+            same_words.append(i)                                       #добавление прошедшего проверку значения в список вывода
     return same_words
+
+
 result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
 result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
-
-print(result1)
-print(result2)
+print(*result1)
+print(*result2)
